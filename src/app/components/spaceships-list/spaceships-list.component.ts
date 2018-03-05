54dc4c6cf95783b35ae68a38febc9ea3bf10c7d7
Diana Tokarska <tokarskadiana@gmail.com>
2018-03-05 11:22:43 +0100

    Generate spaceship-list component

---
+++ src/app/components/spaceships-list/spaceships-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spaceships-list',
+  templateUrl: './spaceships-list.component.html',
+  styleUrls: ['./spaceships-list.component.css']
+})
+export class SpaceshipsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
